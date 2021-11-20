785d3b0ec96072d5f2fe53225068be623114ef85
Vignesh Duraisamy <vigneshd332@gmail.com>
2021-11-20 18:38:05 +0530

    feat: Initilize Basic Features

---
+++ pages/components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './style.module.css';
+
+const NavBar = () => {
+    return (
+        <div className={style.navbar}>
+        CSwap
+        </div>
+    )
+}
+
+export default NavBar
